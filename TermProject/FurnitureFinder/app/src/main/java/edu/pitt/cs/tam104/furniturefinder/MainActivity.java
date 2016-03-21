bac18210c584efc6aa3ef5abb7630ed6f4223d67
haburka <edios124@gmail.com>
2016-03-21 16:22:18 -0400

    maybe this works tiff

--- TermProject/FurnitureFinder/app/src/main/java/edu/pitt/cs/tam104/furniturefinder/MainActivity.java
+++ TermProject/FurnitureFinder/app/src/main/java/edu/pitt/cs/tam104/furniturefinder/MainActivity.java
@@ -3,14 +3,16 @@ package edu.pitt.cs.tam104.furniturefinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTabHost;
-public class MainActivity extends AppCompatActivity {
 
-    protected FragmentTabHost mTabHost;
+public class MainActivity extends FragmentActivity
+{
+    private FragmentTabHost mTabHost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
         mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
