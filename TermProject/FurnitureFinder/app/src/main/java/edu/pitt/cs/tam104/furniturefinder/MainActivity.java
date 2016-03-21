0f268c897f3bae6baed3ca35bac3a3901fa25bc5
haburka <edios124@gmail.com>
2016-03-21 11:36:03 -0400

    cherry picked tiff

--- TermProject/FurnitureFinder/app/src/main/java/edu/pitt/cs/tam104/furniturefinder/MainActivity.java
+++ TermProject/FurnitureFinder/app/src/main/java/edu/pitt/cs/tam104/furniturefinder/MainActivity.java
@@ -2,12 +2,26 @@ package edu.pitt.cs.tam104.furniturefinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 public class MainActivity extends AppCompatActivity {
 
+    protected FragmentTabHost mTabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab1").setIndicator("Map", null),
+                MapTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab2").setIndicator("Favorites", null),
+                FavoriteTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab3").setIndicator("Manage Listings", null),
+                ManageTab.class, null);
     }
 }
