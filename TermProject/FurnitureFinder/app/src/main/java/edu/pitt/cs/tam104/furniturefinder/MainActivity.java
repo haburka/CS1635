c2b2cd311230eab745c648bb76e7a9f638267486
haburka <edios124@gmail.com>
2016-03-22 16:00:05 -0400

    ugh

--- TermProject/FurnitureFinder/app/src/main/java/edu/pitt/cs/tam104/furniturefinder/MainActivity.java
+++ TermProject/FurnitureFinder/app/src/main/java/edu/pitt/cs/tam104/furniturefinder/MainActivity.java
@@ -1,29 +1,23 @@
 package edu.pitt.cs.tam104.furniturefinder;
 
-
-import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTabHost;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends FragmentActivity
-{
-    private FragmentTabHost mTabHost;
-
+    protected FragmentTabHost mTabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
         mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
 
-
         mTabHost.addTab(
                 mTabHost.newTabSpec("tab1").setIndicator("Map", null),
-                MapFragment.class, null);
+                MapTab.class, null);
         mTabHost.addTab(
                 mTabHost.newTabSpec("tab2").setIndicator("Favorites", null),
                 FavoriteTab.class, null);
